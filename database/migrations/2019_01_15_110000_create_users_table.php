42b4ad8032941b1b22aa53e76f41f5a98c9b0709
ArthurFrade <113649440+ArthurFrade1@users.noreply.github.com>
2023-10-20 12:40:07 -0300

    commit

--- database/migrations/2019_01_15_110000_create_users_table.php
+++ database/migrations/2019_01_15_110000_create_users_table.php
@@ -19,10 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->unsignedInteger('role_id');
             $table->rememberToken();
             $table->timestamps();
-
+            
+            $table->unsignedInteger('role_id');
             $table->foreign('role_id')->references('id')->on('roles');
         });
     }
--- resources/views/pages/forms/regular.blade.php
+++ resources/views/pages/forms/regular.blade.php
@@ -3,20 +3,13 @@
 'folderActive' => 'forms',
 'elementActive' => 'regular-forms'
 ])
-
-
 <!-- TELA DE CADASTRO DE FORMULARIO -->
 @section('content')
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
-<br>
-<br>
 <br>
 <br>
 <br>
 <div class="col-md-12">
-    <div class="card">
+    <div class="card ">
         <div class="card-header ">
             <h4 class="card-title">Cadastrar aluno</h4>
         </div>
@@ -25,7 +18,7 @@
                 <!-- Dados Pessoais -->
                 <div class="row">
                     <label class="col-sm-2 col-form-label">Nome</label>
-                    <div class="col-sm-5">
+                    <div class="col-sm-10">
                         <div class="form-group">
                             <input type="text" class="form-control" name="Nome">
                         </div>
@@ -33,7 +26,7 @@
                 </div>
                 <div class="row">
                     <label class="col-sm-2 col-form-label">Sobrenome</label>
-                    <div class="col-sm-5">
+                    <div class="col-sm-10">
                         <div class="form-group">
                             <input type="text" class="form-control" name="Sobrenome">
                         </div>
@@ -41,7 +34,7 @@
                 </div>
                 <div class="row">
                     <label class="col-sm-2 col-form-label">Idade</label>
-                    <div class="col-sm-1">
+                    <div class="col-sm-10">
                         <div class="form-group">
                             <input type="number" class="form-control" name="Idade">
                         </div>
@@ -71,7 +64,7 @@
                 <!-- Contato -->
                 <div class="row">
                     <label class="col-sm-2 col-form-label">Número de telefone</label>
-                    <div class="col-sm-3">
+                    <div class="col-sm-10">
                         <div class="form-group">
                             <input type="text" class="form-control" name="numeroTelefone">
                         </div>
@@ -84,42 +77,30 @@
                         <h4 class="card-title">Endereço</h4>
                     </div>
                     <div class="card-body ">
-                        <div class="row">
+                    <div class="row">
                             <label class="col-sm-2 col-form-label">CEP</label>
-                            <div class="col-sm-2">
+                            <div class="col-sm-10">
                                 <div class="form-group">
                                     <input type="text" class="form-control" name="CEP">
                                 </div>
                             </div>
                         </div>
-
-                        <div class="row">
-                            <label class="col-sm-2 col-form-label">Cidade</label>
-                            <div class="col-sm-5">
-                                <div class="form-group">
-                                    <input type="text" class="form-control" name="localidade">
-                                </div>
-                            </div>
-                        </div>
-
                         <div class="row">
                             <label class="col-sm-2 col-form-label">Rua</label>
-                            <div class="col-sm-5">
+                            <div class="col-sm-10">
                                 <div class="form-group">
-                                    <input type="text" class="form-control" name="logradouro">
+                                    <input type="text" class="form-control" name="Rua">
                                 </div>
                             </div>
                         </div>
-
                         <div class="row">
-                            <label class="col-sm-2 col-form-label">Bairro</label>
-                            <div class="col-sm-5">
+                            <label class="col-sm-2 col-form-label">Cidade</label>
+                            <div class="col-sm-10">
                                 <div class="form-group">
-                                    <input type="text" class="form-control" name="bairro">
+                                    <input type="text" class="form-control" name="Cidade">
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
                 <!-- Escola -->
@@ -127,20 +108,19 @@
                     <div class="card-header ">
                         <h4 class="card-title">Escola</h4>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body ">
                         <div class="row">
                             <label class="col-sm-2 col-form-label">Selecione a escola</label>
                             <div class="col-sm-10">
                                 <div class="form-group">
                                     <select name="select">
-                                        @foreach ($escolas as $escola)
-                                            <option value="{{ $escola->id }}">{{ $escola->nome }}</option>
-                                        @endforeach
+                                        <option value="">Escola1 </option>    
+                                
                                     </select>
                                 </div>
                             </div>
                         </div>
-                    </div>                    
+                    </div>
                 </div>
                 <!-- NECESSIDADES -->
                 <div class="card ">
@@ -166,117 +146,23 @@
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <label class="col-sm-2 col-form-label">Patologia</label>
-                        <div class="col-sm-3">
-                            <div class="form-group">
-                                <input type="text" class="form-control" name="cidade">
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="card ">
-                    <div class="card-header ">
-                        <h4 class="card-title">CID</h4>
-                    </div>
+                    
                     <div class="row">
                         <label class="col-sm-2 col-form-label">CID</label>
-                        <div class="col-sm-3">
+                        <div class="col-sm-10">
                             <div class="form-group">
-                                <input type="text" class="form-control" name="codigo_cid" id="codigo_cid"
-                                    placeholder="Pesquisar por nome ou código CID">
+                                <input type="text" class="form-control" name="cep">
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="card-footer ">
-                    <button type="submit" class="btn btn-info btn-round">Cadastrar</button>
-                </div>
         </div>
+
+        <div class="card-footer ">
+            <button type="submit" class="btn btn-info btn-round">Cadastrar</button>
+        </div>
+        </form>
     </div>
 </div>
-
-
-</form>
 </div>
-</div>
-</div>
-
-
-<script>
-    $('input[name="CEP"]').on('blur', function () {
-        function limpa_formulario_cep() {
-            $('input[name="logradouro"]').val('');
-            $('input[name="localidade"]').val('');
-            $('input[name="bairro"]').val('');
-        }
-
-        var cep = $(this).val();
-        var cep_exibido = cep;
-        cep = cep.replace(/-/g, '');
-
-        if (cep.length <= 8) {
-            cep = cep.replace(/(\d{5})(\d{3})/, '$1-$2');
-        }
-        $(this).val(cep);
-
-        if (cep_exibido.match(/^\d{8}$/)) {
-            limpa_formulario_cep();
-
-            $.getJSON("https://viacep.com.br/ws/" + cep_exibido + "/json/?callback=?", function (dados) {
-                if (!("erro" in dados)) {
-                    $('input[name="logradouro"]').val(dados.logradouro);
-                    $('input[name="bairro"]').val(dados.bairro);
-                    $('input[name="localidade"]').val(dados.localidade);
-                } else {
-                    limpa_formulario_cep();
-                    alert("CEP não encontrado.");
-                }
-            });
-        } else {
-            limpa_formulario_cep();
-            alert("Formato de CEP inválido.");
-        }
-    });
-</script>
-<script>
-    var j = jQuery.noConflict();
-
-    j(document).ready(function() {
-        j("#codigo_cid").autocomplete({
-            source: function(request, response) {
-                j.ajax({
-                    url: "http://localhost:8080/cid10/",
-                    dataType: "json",
-                    data: {
-                        termo: request.term
-                    },
-                    success: function(data) {
-                        var term = request.term.toLowerCase();
-                        var filteredData = data.filter(function(item) {
-                
-                            return item.nome.toLowerCase().includes(term) || item.codigo.toLowerCase().includes(term);
-                        }).map(function(item) {
-                            return {
-                                label: item.codigo + ": " + item.nome,
-                                value: item.codigo + ": " + item.nome
-                            };
-                        });
-
-                        var maxResults = 5;
-                        response(filteredData.slice(0, maxResults));
-                    }
-                });
-            },
-            minLength: 3,
-            select: function(event, ui) {
-                j("#codigo_cid").val(ui.item.value);
-            }
-        });
-    });
-</script>
-
-
 @endsection
